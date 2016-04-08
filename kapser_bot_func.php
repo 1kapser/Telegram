71876ee9686d7849419c859a24432ed6f1d63d7e
1kapser <17kapser@mail.ru>
2016-04-08 15:50:15 +0300

    Update kapser_bot_func.php

--- kapser_bot_func.php
+++ kapser_bot_func.php
@@ -1,10 +1,7 @@
-<?php
-
 $bot_id = 'AAEC_vlWicWGVm0H973Z2XEjvevoInmy4cs';
-
 $chat_id ='217117994';
-
 $url = 'https://api.telegram.org/bot' . $bot_id .
+
 '/sendMessage?text='.urlencode($text).
 '&chat_id='.intval($chat_id);
 $result = file_get_contents($url);
